a03c3abce8a7315dedee413aba1e6363648a9d2d
adarraji <a.darraji@outlook.com>
2023-03-27 12:46:23 -0400

    Added hotel get, get all, delete

--- routes/hotels.js
+++ routes/hotels.js
@@ -26,8 +26,34 @@ router.put("/:id", async (req, res) => {
 })
 
 // DELETE
+router.delete("/:id", async (req, res) => {
+    try {
+        await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has been deleted.");
+    } catch (err) {
+        next(err);
+    }
+})
+
 // GET
+router.get("/:id", async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel);
+    } catch (err) {
+        next(err);
+    }
+})
+
 // GET ALL
+router.get("/", async (req, res) => {
+    try {
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+    } catch (err) {
+        next(err);
+    }
+})
 
 
 export default router
