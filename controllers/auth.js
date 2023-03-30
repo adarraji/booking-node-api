26cad9937f960217b658213ec2bc0bbf18df5765
adarraji <a.darraji@outlook.com>
2023-03-30 19:17:18 -0400

    hash passowrd

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,16 @@
 import User from "../models/User.js"
+import bcrypt from "bcryptjs"
 
 export const resgiter = async (req, res, next) => {
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
     try {
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hash
         })
 
         await newUser.save()
