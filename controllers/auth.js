3e1d766ff2f30f88acd6f93a18831bc807f2428e
adarraji <a.darraji@outlook.com>
2023-03-30 09:34:27 -0400

    created user register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
-export const resgiter = (req, res, next) => {
+import User from "../models/User.js"
+
+export const resgiter = async (req, res, next) => {
     try {
-            
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+
+        await newUser.save()
+        res.status(200).json("User has benn created")
+
     } catch (err) {
         next(err)
     }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { resgiter } from "../controllers/auth";
+import { resgiter } from "../controllers/auth.js";
 const router = express.Router();
 
-router.get("/register", resgiter)
+router.post("/register", resgiter)
 
 export default router
