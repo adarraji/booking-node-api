39b3bdadb04e2f9c845a199f663f23dc7c9dbc6a
adarraji <a.darraji@outlook.com>
2023-03-30 21:51:59 -0400

    created user routes

--- routes/uesrs.js
+++ routes/uesrs.js
@@ -1,5 +1,13 @@
 import express from "express";
+import User from "../models/User.js";
+import { createError } from "../utils/error.js";
+import { createUser, deleteUser, getUser, getUsers, updateUser } from "../controllers/User.js";
 const router = express.Router();
 
 
-export default router
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.get("/:id", getUser);
+router.get("/", getUsers);
+
+export default router;
