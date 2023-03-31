69d7d439cb242783c094e0d21626f40c1bdb5a98
adarraji <a.darraji@outlook.com>
2023-03-30 21:43:31 -0400

    using JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
 
@@ -30,7 +31,10 @@ export const login = async (req, res, next) => {
         const isPassswordCorrect = await bcrypt.compare(req.body.password, user.password);
         if (!isPassswordCorrect) return next(createError(400, "Wrong password or username!"));
 
+        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT);
+
         const { password, isAdmin, ...otherDetails } = user._doc
+
         res.status(200).json(otherDetails)
     } catch (err) {
         next(err);
