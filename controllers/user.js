75791808cff37ebd8ef5c961d3e69ced534d6ccb
adarraji <a.darraji@outlook.com>
2023-04-16 03:05:35 -0400

    fix

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+import User from "../models/User.js"
+
 export const updateUser = async (req, res, next) => {
     try {
         const updatedUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
