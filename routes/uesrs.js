7422be3b5c1db8d66bfd18da9be661ab6dd7366c
adarraji <a.darraji@outlook.com>
2023-04-19 21:54:30 -0400

    verifytoken

--- routes/uesrs.js
+++ routes/uesrs.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
-import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 const router = express.Router();
 
 // router.get("/checkauthentication", verifyToken, (req, res, next) => {
@@ -16,9 +16,9 @@ const router = express.Router();
 // })
 
 
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.get("/:id", getUser);
-router.get("/", getUsers);
+router.put("/:id", verifyUser, updateUser);
+router.delete("/:id", verifyUser, deleteUser);
+router.get("/:id", verifyUser, getUser);
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
