c3bf6346fac37b20169d2d3fd6f0c42df799a4be
adarraji <a.darraji@outlook.com>
2023-04-30 23:26:20 -0400

    added count hotels  by type

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { CountByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel, countByType } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
@@ -9,7 +9,7 @@ router.put("/:id", verifyAdmin, updateHotel);
 router.delete("/:id", verifyAdmin, deleteHotel);
 router.get("/find/:id", getHotel);
 router.get("/", getHotels);
-router.get("/CountByCity", CountByCity);
-router.get("/CountByType", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
 
 export default router;
